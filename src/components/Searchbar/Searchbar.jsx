4c3bd6b6b315d28e3ad51dabf37cbe06df9db057
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-13 23:32:56 +0200

    Searchbar
    23-32

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,49 +4,49 @@ import './Searchbar.css';
 import PropTypes from 'prop-types';
 
 export class Searchbar extends Component {
-    state = {
-        searchQuery: '',
-    };
-    
-    handleChange = e => {
-        
-        this.setState({ searchQuery: e.currentTarget.value.toLowerCase() });
-    };
-    
-    handleSubmit = e => {
-        e.preventDefault();
-        if (this.state.searchQuery.trim() === '') {
-            return alert('Please enter something :)');
-        }
-        this.props.onSubmit(this.state.searchQuery);
-        this.setState({ searchQuery: '' });
-    };
-    
-    render() {
-        return (
-            <header className='Searchbar'>
-                <form onSubmit={this.handleSubmit} className="SearchForm">
-                    <button type="submit" className="SearchForm-button">
-                        <span>
-                            <FiSearch size={25} stroke="#3f51b5" />
-                        </span>
-                    </button>
-                    <input
-                        className="SearchForm-input "
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                        name="searchQuery"
-                        value={this.state.searchQuery}
-                        onChange={this.handleChange}
-                    />
+  state = {
+    searchQuery: '',
+  };
 
-                </form>
+  handleChange = e => {
+    // const { name, value } = e.currentTarget;
+    // this.setState({ [name]: value });
+    this.setState({ searchQuery: e.currentTarget.value.toLowerCase() });
+  };
 
-            </header>
-        );
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.searchQuery.trim() === '') {
+      return alert('Please enter something :)');
     }
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({ searchQuery: '' });
+  };
+
+  render() {
+    return (
+      <header className="Searchbar">
+        <form onSubmit={this.handleSubmit} className="SearchForm">
+          <button type="submit" className="SearchForm-button">
+            <span>
+              <FiSearch size={25} stroke="#3f51b5" />
+            </span>
+          </button>
+
+          <input
+            className="SearchForm-input "
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            name="searchQuery"
+            value={this.state.searchQuery}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
 }
 Searchbar.propTypes = {
   onSubmit: PropTypes.func,
